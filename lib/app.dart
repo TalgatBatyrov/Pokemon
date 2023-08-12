f37fba3d1f78cf38970a8bd3f75b704e26648235
Talgat <talgatbatyrov7@gmail.com>
2023-08-12 19:35:27 +0600

    Get started

---
+++ lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon/router/app_router.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final _appRouter = AppRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routerConfig: _appRouter.config(),
+    );
+  }
+}
---
+++ lib/features/pokemons/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:auto_route/annotations.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Demo'),
+      ),
+      body: const Center(
+        child: Text('Hello, world!'),
+      ),
+    );
+  }
+}
