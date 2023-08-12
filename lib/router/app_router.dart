c6e9bf9334b691cac2c0c5b9531567739f039074
Talgat <talgatbatyrov7@gmail.com>
2023-08-12 19:29:46 +0600

    create project

---
+++ lib/router/app_router.dart
@@ -0,0 +1,13 @@
+import 'package:auto_route/auto_route.dart';
+
+import '../features/pokemons/pages/home_page.dart';
+
+part 'app_router.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(page: HomeRoute.page, initial: true),
+      ];
+}
